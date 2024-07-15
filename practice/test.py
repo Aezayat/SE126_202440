be91356ca2c49f11a9a636cc8c055a0861b509c2
Zayat <aezayat@email.neit.edu>
2024-07-15 10:18:24 -0400

    w1d1 practice test

---
+++ practice/test.py
@@ -0,0 +1,13 @@
+#hello this is my comment!
+#Your Name, Lab #, & Date 
+#Variable Dictionary
+
+
+#-----------MAIN CODE---------------------------------------
+
+#-----------FUNCTIONS---------------------------------------
+
+
+#---------- IMPORTS-----------------------------------------
+
+print("Hello World!:") 
