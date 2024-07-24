f0612c45d2f0f1448b78a1ef0b429893fa17c2b3
Zayat <aezayat@email.neit.edu>
2024-07-24 09:39:23 -0400

    w2d2_lab2a

--- week2_practice/w2d2_lab2a.py
+++ week2_practice/w2d2_lab2a.py
@@ -8,7 +8,7 @@ current_people = 0
 
 total_records = 0
 
-print(f"ROOM\t\tMAX\t\tMIN\t\tOVER")
+print(f"ROOM\t\t\t MAX\t\t\tMIN\t\t\tOVER")
 
 with open("week2_practice/lab2a.csv") as csvfile:
 
@@ -18,5 +18,5 @@ with open("week2_practice/lab2a.csv") as csvfile:
         total_records += 1
         if record[2] > record[1]:
 
-            print(f"{record[0]}\t{record[2]}\t{record[1]}")
+            print(f"{record[0]}\t\t{record[2]}\t\t{record[1]}")
             
